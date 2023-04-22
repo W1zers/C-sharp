d54c48438950165a2ac07b72d084e20dd81f4e5b
Ramin Gambarov <ramin.gambarov2014@gmail.com>
2023-04-22 12:37:54 +0300

    seme/hometask

---
+++ task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+// Напишите программу, которая принимает на вход трёхзначное число и на выходе
+// показывает последнюю цифру этого числа. Например, 456->6 782->2 918->8
+
+Console.WriteLine ("Введите трехзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+// if(number < 0)
+// {
+//  number = -number ;
+// }
+int lastNumber = 0;
+
+if (number > 99 && number < 1000 )
+{
+    lastNumber = number % 10 ;
+    Console.Write($"Последеняя цифра числа {number} = {lastNumber}");
+}
+else
+{
+   Console.Write ("Число не трехзначное");
+}
